8f18040c32e4d0707462f2f61204e61689648b1f
ArthurMathis <mathis.arthur68@gmail.com>
2023-09-09 15:10:47 +0200

    Mise-à-jour du script preload.js : ajout des fichiers images contenus dans le slider principal ainsi que dans les affiches de titres.

--- script/preload.js
+++ script/preload.js
@@ -1,7 +1,17 @@
 let resourcesToPreload = [
     { type: 'style', url: 'style/main.css' },
     { type: 'script', url: 'script/animation.js' },
-    { type: 'script', url: 'script/app.js' }
+    { type: 'script', url: 'script/app.js' },
+    { type: 'image', url: 'src/img/Titres/ABout_ME_ILLU.png' },
+    { type: 'image', url: 'src/img/Titres/Branding.png' },
+    { type: 'image', url: 'src/img/Titres/Illustration.png' },
+    { type: 'image', url: 'src/img/Titres/Motion.png' },
+    { type: 'image', url: 'src/img/Titres/Typographie.png' },
+    { type: 'image', url: 'src/img/Titres/UX-UI_DESIGN.png' },
+    { type: 'image', url: 'src/img/Typographie/cover-mockup-paysage.jpg' },
+    { type: 'image', url: 'src/img/Illustration/Recette.jpg' },
+    { type: 'image', url: 'src/img/Branding/Magasin.jpg' },
+    { type: 'image', url: 'src/img/UI:UX Design/Free_Iphone_14_Pro_Mockup_4.jpg' }
 ];
 
 //{ type: 'style', url: 'style/main.css' },
@@ -12,27 +22,49 @@ function preloadResources() {
     let promises = [];
 
     resourcesToPreload.forEach(resource => {
-        let element;
-        if (resource.type === 'style') {
-            element = document.createElement('link');
-            element.rel = 'stylesheet';
-            element.href = resource.url;
-        } else if (resource.type === 'script') {
-            element = document.createElement('script');
-            element.type = 'text/javascript';
-            element.src = resource.url; // Utilisez src pour les fichiers JavaScript
-        }
+        let promise;
 
-        document.head.appendChild(element);
-        let promise = new Promise(resolve => {
-            element.onload = resolve;
-        });
+        switch (resource.type) {
+            case 'style':
+                let styleElement = document.createElement('link');
+                styleElement.rel = 'stylesheet';
+                styleElement.href = resource.url;
+                document.head.appendChild(styleElement);
+                promise = new Promise(resolve => {
+                    styleElement.onload = resolve;
+                });
+                break;
+
+            case 'script':
+                let scriptElement = document.createElement('script');
+                scriptElement.type = 'text/javascript';
+                scriptElement.src = resource.url;
+                document.head.appendChild(scriptElement);
+                promise = new Promise(resolve => {
+                    scriptElement.onload = resolve;
+                });
+                break;
+
+            case 'image':
+                promise = new Promise((resolve, reject) => {
+                    let img = new Image();
+                    img.src = resource.url;
+                    img.onload = resolve;
+                    img.onerror = reject;
+                });
+                break;
+
+            default:
+                console.error('Type de ressource non pris en charge :', resource.type);
+                break;
+        }
 
         promises.push(promise);
     });
 
     return Promise.all(promises);
 }
+
 
 preloadResources().then(() => {
     let endTime = Date.now();
