0d6be441a5ae67a3bf325fa0ef6028a4dc04ddd1
ArthurMathis <mathis.arthur68@gmail.com>
2023-09-14 20:46:53 +0200

    Patch correctif du menu déroulant -> passage en javascript

--- script/my-components.js
+++ script/my-components.js
@@ -188,7 +188,7 @@ class menu{
 
         this.liens.forEach((element) => {
             element.addEventListener('click', (e) => {
-                //e.preventDefault();
+                e.preventDefault();
                 this.onSelectionne(element);
             });
         });
@@ -214,7 +214,7 @@ class menu{
         this.entete.classList.remove('not-focused');
         this.isClicked = true;
 
-        /*
+
         let href = link.getAttribute('href');
         if (href) {
             let targetId = href.substring(1);
@@ -228,6 +228,6 @@ class menu{
         } else {
             console.log("La source du lien est introuvable");
         }
-        */
+
     }
 }
