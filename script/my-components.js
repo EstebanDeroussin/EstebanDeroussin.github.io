b777c4a07417d8a462c0aa41a3cb9fd8f9e075d5
ArthurMathis <mathis.arthur68@gmail.com>
2023-09-26 08:23:11 +0200

    Patch correctif menu déroulant

--- script/my-components.js
+++ script/my-components.js
@@ -188,7 +188,7 @@ class menu{
 
         this.liens.forEach((element) => {
             element.addEventListener('click', (e) => {
-                /*e.preventDefault();*/
+                e.preventDefault();
                 this.onSelectionne(element);
             });
         });
@@ -215,7 +215,6 @@ class menu{
         this.isClicked = true;
 
 
-        /*
         let href = link.getAttribute('href');
         if (href) {
             let targetId = href.substring(1);
@@ -228,7 +227,6 @@ class menu{
             }
         } else {
             console.log("La source du lien est introuvable");
-        }
-        */
+        }    
     }
 }
